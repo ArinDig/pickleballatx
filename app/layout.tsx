1ed952b8082f94d197644544eb323906b260de3f
Arin Brown <arin@arindigital.com>
2026-04-25 00:11:24 -0500

    Remove Monetag

--- app/layout.tsx
+++ app/layout.tsx
@@ -156,14 +156,7 @@ export default function RootLayout({
       </head>
       <body className="flex min-h-screen flex-col antialiased">
         <GoogleAnalytics />
-        <Script
-          src="https://quge5.com/88/tag.min.js"
-          data-zone="233330"
-          async
-          data-cfasync="false"
-          strategy="afterInteractive"
-        />
-        <Header />
+<Header />
         <main className="flex-1">
           {children}
         </main>
